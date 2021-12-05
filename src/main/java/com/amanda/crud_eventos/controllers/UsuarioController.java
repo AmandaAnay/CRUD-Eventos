bb3e66793f947b942343040c3ea99d48dadb3891
Amanda <amandinha06@hotmail.com>
2021-12-04 22:21:55 -0300

    Adicionado Usuario Controller

--- src/main/java/com/amanda/crud_eventos/controllers/UsuarioController.java
+++ src/main/java/com/amanda/crud_eventos/controllers/UsuarioController.java
@@ -1,4 +1,52 @@
 package com.amanda.crud_eventos.controllers;
 
+import com.amanda.crud_eventos.dto.UsuarioDTO;
+import com.amanda.crud_eventos.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "login") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<UsuarioDTO> list = usuarioService.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> criar(@RequestBody UsuarioDTO dto) {
+        dto = usuarioService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable Long id, @RequestBody UsuarioDTO dto) {
+        dto = usuarioService.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
